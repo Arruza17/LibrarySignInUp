8ee2658bf5d6b76f4840c750c5d23d464529ea96
adriliko28@gmail.com <>
2021-10-25 13:49:46 +0200

    User Singleton class, Enums UserPrivilege and UserStatus finished.

---
+++ src/enumerations/UserPrivilege.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumerations;
+
+/**
+ *
+ * @author Adrian Perez
+ */
+    public enum UserPrivilege  {
+
+    /**
+     *Grants all the privileges to the user.
+     */
+    ENABLED,
+
+    /**
+     *Removes all the privileges of the user.
+     */
+    DISABLED;
+    }
+
