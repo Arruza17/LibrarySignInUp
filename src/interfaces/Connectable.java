03d232c8ff09074697e8f4cfdbf62029469826f7
Yeray Sampedro <yeraysampedro@gmail.com>
2021-10-28 10:32:22 +0200

    Connectable interface issue solved

--- src/interfaces/Connectable.java
+++ src/interfaces/Connectable.java
@@ -8,24 +8,27 @@ package interfaces;
 import model.User;
 
 import exceptions.ServerDownException;
+import model.DataEncapsulator;
 
 /**
- * This interface contains the methots that allow both
- * model and logic connect
+ * This interface contains the methots that allow both model and logic connect
+ *
  * @author Ander Arruza
  */
 public interface Connectable {
-    
+
     /**
      * This method signs In the user
+     *
      * @param user the user to be set
      */
-    public void signIn(User user) throws ServerDownException;
-    
+    public DataEncapsulator signIn(User user) throws ServerDownException;
+
     /**
      * This method signs Up the user
+     *
      * @param user
      */
     public void signUp(User user) throws ServerDownException;
-    
+
 }
