b05ff630f9936cf3c14f5e6b53cbc086179c6436
2dam <2dam@HZ375757.TartangaLH.eus>
2021-11-11 10:34:44 +0100

    Connectable javadoc finish

--- src/interfaces/Connectable.java
+++ src/interfaces/Connectable.java
@@ -22,13 +22,17 @@ public interface Connectable {
      * This method signs In the user
      *
      * @param user the user to be set
+     * @return the DataEncapsulator object
+     * @throws exceptions.ServerDownException if the server is down 
      */
     public DataEncapsulator signIn(User user) throws ServerDownException;
 
     /**
      * This method signs Up the user
      *
-     * @param user
+     * @param user the user to be signed up
+     * @throws exceptions.ServerDownException if the server is down
+     * @throws exceptions.LoginFoundException if the user already exits
      */
     public void signUp(User user) throws ServerDownException, LoginFoundException;
 
