e51ba4a3080f69e5dd276effbae2e303f6741d10
yeraysampedro <yeraysampedro@gmail.com>
2021-11-15 15:32:38 +0100

    Yeray Eval Change

--- src/exceptions/MaxCharactersException.java
+++ src/exceptions/MaxCharactersException.java
@@ -10,7 +10,7 @@ public class MaxCharactersException extends Exception{
      *Constructs a new exception with the specified detail message. Used to control that the characters are not more than 255
      */
     public MaxCharactersException() {
-        super("The maximum amount of characters is 255, try again with shorter information");
+        super("The maximum amount of characters is \n255, try again with shorter information");
     }
     
 }
